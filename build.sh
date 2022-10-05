0e594e9fec93ebdadd8ae795a5f58a28cef9e30d
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-05 16:45:09 +0300

    yap

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
-#sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
+# Eşitle
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b ProjectBlaze-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# Yapı
 source build/envsetup.sh
-lunch cherish_lava-userdebug
-#export WITH_GMS=true
-#export TARGET_USES_MINI_GAPPS=true
-export CHERISH_VANILLA=true
+lunch blaze_lava-userdebug
+export WITH_GAPPS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka
 mka bacon
-# end
