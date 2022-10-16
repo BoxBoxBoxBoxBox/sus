f5024c2a39d8b449d30ed931611f8e5f74417c3e
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-16 20:37:30 +0300

    Again

--- script/clone.sh
+++ script/clone.sh
@@ -6,5 +6,5 @@ rm -rf packages/apps/Settings
 git clone --depth 1 https://github.com/orkunergun/packages_apps_Settings -b tm packages/apps/Settings
 rm -rf vendor/proton
 git clone --depth 1 https://github.com/orkunergun/vendor_proton -b tm vendor/proton
-rm -rf packages_apps_Updater
+rm -rf packages/apps/Updater
 git clone --depth 1 https://github.com/orkunergun/ProtonPlusUpdater -b tm packages/apps/Updater
