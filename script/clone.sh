3aea6f83490269e3c411754cddc7343f1878bb75
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-16 20:04:15 +0300

    [skip CI]

---
+++ script/clone.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf frameworks/base
+git clone --depth 1 https://github.com/orkunergun/frameworks_base -b tm frameworks/base
+rm -rf packages/apps/Settings
+git clone --depth 1 https://github.com/orkunergun/packages_apps_Settings -b tm packages/apps/Settings
+rm -rf vendor/proton
+git clone --depth 1 https://github.com/orkunergun/vendor_proton -b tm vendor/proton
+rm -rf packages_apps_Updater
+git clone --depth 1 https://github.com/orkunergun/ProtonPlusUpdater -b tm packages/apps/Updater
