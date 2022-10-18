5be84c1c6573304d3faa97c3adfd6a7e7defe7a6
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-18 19:45:52 +0300

    H

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b 13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_merlinx-userdebug
+lunch cherish_lava-userdebug
+export WITH_GMS=true
+#export CHERISH_VANILLA=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
-if [ ! -e out/target/product/*/recovery.img ]; then # you don't have to run this you're not facing oom kill issue while build is about 98-98%
-mka bacon # re-run the build cuz there's still time left considering only few targets left
+make bacon -j$(nproc --all)
+if [ ! -e out/target/product//*2022.zip ]; then # you don't have to run this you're not facing oom kill issue while build is about 98-98%
+make bacon -j$(nproc --all) # re-run the build cuz there's still time left considering only few targets left
 fi
 #end
