7b6c06a45733b04e262ec357b4364ce726f3edf3
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-19 19:57:22 +0300

    [skip CI]

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b crDroid-12.1-S .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch cherish_lava-userdebug
-export WITH_GMS=true
-#export CHERISH_VANILLA=true
+lunch lineage_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-make bacon -j$(nproc --all)
+make GameSpace
 if [ ! -e out/target/product//*2022.zip ]; then # you don't have to run this you're not facing oom kill issue while build is about 98-98%
-make bacon -j$(nproc --all) # re-run the build cuz there's still time left considering only few targets left
+make GameSpace # re-run the build cuz there's still time left considering only few targets left
 fi
 #end
