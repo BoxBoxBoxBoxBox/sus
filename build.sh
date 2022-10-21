2681c6a27b46dac6b304f1b8f31ca6bf935d4674
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-21 20:13:27 +0300

    [skip CI]

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init -u https://github.com/PixelOS-Pixelish/manifest.git -b thirteen
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b PixelOS-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/crdroid-lava/Manifest.git --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lava-userdebug
+lunch lineage_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon -j$(nproc --all)
