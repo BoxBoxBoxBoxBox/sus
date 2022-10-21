914413fc581fabd3149c0f4f29d54cc6da364330
BoxBoxBoxBoxBox <110622134+BoxBoxBoxBoxBox@users.noreply.github.com>
2022-10-21 20:46:30 +0300

    Uf

--- script/clone.sh
+++ script/clone.sh
@@ -1,4 +1,4 @@
 rm -rf vendor/crDroidOTA
 rm -rf packages/apps/Updater
-git clone --depth 1 orkunergun/crDroidOTA -b 12.1 vendor/crDroidOTA
-git clone --depth 1 orkunergun/crDroid-Updater -b 12.1 packages/apps/Updater
+git clone --depth 1 github.com/orkunergun/crDroidOTA -b 12.1 vendor/crDroidOTA
+git clone --depth 1 github.com/orkunergun/crDroid-Updater -b 12.1 packages/apps/Updater
